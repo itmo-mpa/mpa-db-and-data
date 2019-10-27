d17ba67e0003a0eeeea72e0e41acbe9c010cd5ad
vvllaadd <vvllaadd99@yandex.ru>
2019-10-27 15:38:13 +0300

    substitute string enum literals in predicates with their ids

--- statemachine.sql
+++ statemachine.sql
@@ -78,31 +78,31 @@ insert into state(id, name, description, disease_id) values (20, 'HS12', 'Уто
 
 insert into transition(id, state_from_id, state_to_id, predicate) values (1, 8, 9, 'eq ({status.1}, true)');
 insert into transition(id, state_from_id, state_to_id, predicate) values (2, 9, 10, 'and (eq ({status.2}, true), and (eq ({status.3}, true), gte ({status.4}, 0.4)))');
-insert into transition(id, state_from_id, state_to_id, predicate) values (3, 10, 12, 'eq ({status.5}, “positive”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (4, 10, 11, 'eq ({status.5}, “uncertain”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (5, 11, 12, 'eq ({status.6}, “positive”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (6, 12, 13, 'eq ({status.7}, “positive”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (7, 12, 17, 'and (eq ({status.7, “negative”}), eq ({status.8}, true))');
-insert into transition(id, state_from_id, state_to_id, predicate) values (8, 12, 17, 'and (eq ({status.7, “negative”}), eq ({status.9}, true))');
-insert into transition(id, state_from_id, state_to_id, predicate) values (10, 13, 14, 'eq ({status.10}, “positive”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (11, 13, 17, 'and (eq ({status.10, “negative”}), eq ({status.8}, true))');
-insert into transition(id, state_from_id, state_to_id, predicate) values (12, 13, 17, 'and (eq ({status.10, “negative”}), eq ({status.9}, true))');
-insert into transition(id, state_from_id, state_to_id, predicate) values (13, 15, 3, 'eq ({status.11}, “positive”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (14, 15, 16, 'and (eq ({status.11, “negative”}), eq ({status.9}, true))');
-insert into transition(id, state_from_id, state_to_id, predicate) values (15, 15, 17, 'and (eq ({status.11, “negative”}), eq ({status.8}, true))');
-insert into transition(id, state_from_id, state_to_id, predicate) values (16, 16, 13, 'eq ({status.12}, “indicated”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (17, 16, 4, 'eq ({status.12}, “not indicated”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (18, 17, 4, 'eq ({status.13}, “not indicated”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (19, 17, 18, 'eq ({status.13}, “indicated”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (22, 18, 5, 'eq ({status.14}, “negative”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (23, 18, 19, 'eq ({status.14}, “positive”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (24, 19, 5, 'eq ({status.15}, “negative”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (25, 19, 6, 'eq ({status.15}, “positive”)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (3, 10, 12, 'eq ({status.5}, 1)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (4, 10, 11, 'eq ({status.5}, 3)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (5, 11, 12, 'eq ({status.6}, 4)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (6, 12, 13, 'eq ({status.7}, 6)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (7, 12, 17, 'and (eq ({status.7, 7}), eq ({status.8}, true))');
+insert into transition(id, state_from_id, state_to_id, predicate) values (8, 12, 17, 'and (eq ({status.7, 7}), eq ({status.9}, true))');
+insert into transition(id, state_from_id, state_to_id, predicate) values (10, 13, 14, 'eq ({status.10}, 8)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (11, 13, 17, 'and (eq ({status.10, 9}), eq ({status.8}, true))');
+insert into transition(id, state_from_id, state_to_id, predicate) values (12, 13, 17, 'and (eq ({status.10, 9}), eq ({status.9}, true))');
+insert into transition(id, state_from_id, state_to_id, predicate) values (13, 15, 3, 'eq ({status.11}, 10)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (14, 15, 16, 'and (eq ({status.11, 11}), eq ({status.9}, true))');
+insert into transition(id, state_from_id, state_to_id, predicate) values (15, 15, 17, 'and (eq ({status.11, 11}), eq ({status.8}, true))');
+insert into transition(id, state_from_id, state_to_id, predicate) values (16, 16, 13, 'eq ({status.12}, 12)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (17, 16, 4, 'eq ({status.12}, 13)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (18, 17, 4, 'eq ({status.13}, 15)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (19, 17, 18, 'eq ({status.13}, 14)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (22, 18, 5, 'eq ({status.14}, 17)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (23, 18, 19, 'eq ({status.14}, 16)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (24, 19, 5, 'eq ({status.15}, 19)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (25, 19, 6, 'eq ({status.15}, 18)');
 
 insert into transition(id, state_from_id, state_to_id, predicate) values (26, 8, 1, 'eq ({status.1}, false)');
 insert into transition(id, state_from_id, state_to_id, predicate) values (27, 9, 2, 'not(and (eq ({status.2}, true), and (eq ({status.3}, true), gte ({status.4}, 0.4))))');
-insert into transition(id, state_from_id, state_to_id, predicate) values (28, 10, 2, 'eq ({status.5}, “negative”)');
-insert into transition(id, state_from_id, state_to_id, predicate) values (29, 11, 2, 'eq ({status.6}, “negative”)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (28, 10, 2, 'eq ({status.5}, 2)');
+insert into transition(id, state_from_id, state_to_id, predicate) values (29, 11, 2, 'eq ({status.6}, 5)');
 
 
 commit;
